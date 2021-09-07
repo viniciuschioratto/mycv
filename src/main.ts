ce9d1ac60f50473291d396864525c90c73bc4ddb
Vinicius Chioratto <viniciusmchioratto@gmail.com>
2021-09-07 10:31:54 -0300

    Inserting Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -9,6 +10,17 @@ async function bootstrap() {
       whitelist: true,
     })
   )
+
+  const config = new DocumentBuilder()
+    .setTitle('My cv')
+    .setDescription('The my cv API')
+    .setVersion('1.0')
+    .addTag('mycv')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
