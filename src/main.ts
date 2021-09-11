df48c5842ab7c3c87dd04d44abd79ef3cc2fc7d4
Vinicius Chioratto <viniciusmchioratto@gmail.com>
2021-09-11 15:40:34 -0300

    Integration Testing

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-const cookieSession =  require('cookie-session');
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    cookieSession({
-      keys: ['avsvasva']
-    })
-  )
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    })
-  )
-
   const config = new DocumentBuilder()
     .setTitle('My cv')
     .setDescription('The my cv API')
